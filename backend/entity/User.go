e3519003f3f8ffd4e4190c405fb0d7a8d9629bb2
HEEHAMTARO <jaonai0954400179@gmail.com>
2025-07-29 15:06:27 +0700

    FixEntityUser

--- backend/entity/User.go
+++ backend/entity/User.go
@@ -6,14 +6,13 @@ import (
 )
 
 type User struct {
-	gorm.Model
-	Firstname string    `binding:"required,min=1,max=50"`
-	Lastname  string    `binding:"required,min=1,max=50"`
-	Email     string    `binding:"required,email"`
-	Age       int       `binding:"gte=0,lte=120"`
-	Birthday  time.Time `binding:"required"` // ถ้าไม่อยากบังคับให้ส่งมา ก็ลบ `required`
-	Password  string    `binding:"required,min=6,max=100"`
-	Profile   string    `gorm:"type:longtext" binding:"omitempty"` // optional, สูงสุด 10000 ตัวอักษร  
-	Type string `gorm:"default:user"`
-                     
+    gorm.Model
+    Firstname string    `binding:"omitempty,min=1,max=50"`
+    Lastname  string    `binding:"omitempty,min=1,max=50"`
+    Email     string    `binding:"required,email"`
+    Age       int       `binding:"omitempty,gte=0,lte=120"`
+    Birthday  time.Time `binding:"omitempty"`
+    Password  string    `binding:"required,min=6,max=100"`
+    Profile   string    `gorm:"type:longtext" binding:"omitempty"`
+    Type      string    `gorm:"default:user"`
 }
